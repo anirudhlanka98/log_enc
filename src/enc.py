7c51f2c2db1c5bdc59553f78cdcc0e6fa823de2a
Anirudh <anirudhlanka98@gmail.com>
2018-06-25 17:51:22 +0530

    bug fixes

--- src/enc.py
+++ src/enc.py
@@ -7,30 +7,36 @@ types, fanins, input_values, outputs, seq = readfile(fname)
 
 lk = []
 k = 0
+count = 0
 y = []
 key = []
 lis = []
 
-ran_gat = random.sample(types.keys(),random.randint(1,len(types)))
+ran_gat = random.sample(types.keys(),int(0.1*len(types)))
 
-for i in ran_gat:
-	y = fanins[i]
+while count < 0.1*len(types):
+	ran_gat = random.choice(types.keys())
+	if types[ran_gat] != 'input' and types[ran_gat] != 'keyinput':
+		y = fanins[ran_gat]
+		t = types[ran_gat]
+		kgat = 'enc'+str(k)
+		kin = 'keyinput'+str(k)
+		types.update({kin:'keyinput'})
+		fanins.update({kgat:y})
+		del fanins[ran_gat]
+		fanins.update({ran_gat:[kgat,kin]})
+		if random.randint(0,1) :
+			types.update({ran_gat:'xor'})
+			types.update({kgat:t})
+			key = key + [0]
+		else :
+			types.update({ran_gat:'xnor'})
+			types.update({kgat:t})
+			key = key + [1]
 
-	t = types[i]
-	kgat = 'enc'+str(k)
-	kin = 'keyinput'+str(k)
-	types.update({kin:'keyinput'})
-	fanins.update({kgat:y})
-	del fanins[i]
-	fanins.update({i:[kgat,kin]})
-	if random.randint(0,1) :
-		types.update({i:'xor'})
-		types.update({kgat:t})
-		key = key + [0]
-	else :
-		types.update({i:'xnor'})
-		types.update({kgat:t})
-		key = key + [1]
+	else:
+		continue
+	count = count + 1
 
 	if t == 'input':
 		ind = seq.keys()[seq.values().index(i)]
@@ -48,7 +54,7 @@ for i in range(len(key)):
 	f.write("INPUT(keyinput%d)\n"%i)	
 
 for i in outputs:
-	f.write("OUTPUTS(%s)\n"%i)
+	f.write("OUTPUT(%s)\n"%i)
 
 for i in fanins:
 	if types[i] != 'input':
@@ -60,8 +66,4 @@ for i in fanins:
 		f.write(z)
 		f.write(")\n")
 
-'''print 'Random gates: ', ran_gat, '\n'
-print 'Types: ', types, '\n'
-print 'Fanins', fanins, '\n'
-print 'Keys: ', key
-'''
+		
