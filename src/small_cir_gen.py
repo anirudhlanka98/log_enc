9dfcb338e80ecbacae4080df51b9b3d0e90c07e8
Anirudh <anirudhlanka@Anirudh.local>
2018-07-07 12:05:58 +0530

    Added files

--- src/small_cir_gen.py
+++ src/small_cir_gen.py
@@ -1,75 +1,82 @@
 import random
 import os
+import sys
 
-types = {}
-fanins = {}
-outputs = []
+w = int(sys.argv[1])
+m = 0
+while m < w :
+	types = {}
+	fanins = {}
+	outputs = []
 
-lk = ['AND','OR','NOT','NAND','NOR','XOR','XNOR']
-n_str = input('Enter the number of gates: ')
-n = int(n_str)
-n_in = int(0.25 * n)
-n_op = n - int(0.15 * n)
+	lk = ['AND','OR','NOT','NAND','NOR','XOR','XNOR']
+	s = "Enter the number of gates for circuit ",m,": "
+	n_str = input(s)
+	n = int(n_str)
+	n_in = int(0.25 * n)
+	n_op = n - int(0.15 * n)
 
-for i in range(n_in):
-	types.update({i:'INPUT'})
+	for i in range(n_in):
+		types.update({i:'INPUT'})
 
-k = n_in
-while k <= n:
-	x = random.choice(lk)
-	if x == 'NOT':
-		f = random.choice(types.keys())
-		fanins.update({k:f})
-	else:
-		f = random.sample(types.keys(),2)
-		fanins.update({k:f})
-	types.update({k:x})
-	k = k + 1
+	k = n_in
+	while k <= n:
+		x = random.choice(lk)
+		if x == 'NOT':
+			f = random.choice(types.keys())
+			fanins.update({k:f})
+		else:
+			f = random.sample(types.keys(),2)
+			fanins.update({k:f})
+		types.update({k:x})
+		k = k + 1
 
-for i in range(n_op,n):
-	outputs = outputs + [i]
+	for i in range(n_op,n):
+		outputs = outputs + [i]
 
-def get_nonexistant_path(fname_path):
-    if not os.path.exists(fname_path):
-        return fname_path
-    filename, file_extension = os.path.splitext(fname_path)
-    i = 1
-    new_fname = "{}-{}{}".format(filename, i, file_extension)
-    while os.path.exists(new_fname):
-        i += 1
-        new_fname = "{}-{}{}".format(filename, i, file_extension)
-    return new_fname
+	def get_nonexistant_path(fname_path):
+	    if not os.path.exists(fname_path):
+	        return fname_path
+	    filename, file_extension = os.path.splitext(fname_path)
+	    i = 1
+	    new_fname = "{}-{}{}".format(filename, i, file_extension)
+	    while os.path.exists(new_fname):
+	        i += 1
+	        new_fname = "{}-{}{}".format(filename, i, file_extension)
+	    return new_fname
 
-fname = get_nonexistant_path("small.bench")
+	fname = get_nonexistant_path("small.bench")
 
-f = open(fname,"w+")
+	f = open(fname,"w+")
 
-for i in sorted(types.keys()):
-	if types[i] == 'INPUT':
-		f.write("INPUT(%s)\n"%i)
+	for i in sorted(types.keys()):
+		if types[i] == 'INPUT':
+			f.write("INPUT(%s)\n"%i)
 
-f.write("\n")
+	f.write("\n")
 
-for i in outputs:
-	f.write("OUTPUT(%s)\n"%i)
+	for i in outputs:
+		f.write("OUTPUT(%s)\n"%i)
 
-f.write("\n")
+	f.write("\n")
 
-for i in fanins:
-	if types[i] != 'INPUT' and types[i] != 'NOT':
-		f.write("%s = %s("%(i,types[i]))
-		z = ''
-		for i in fanins[i]:
-			z = z + str(i) + ', '
-		z = z[:-2]
-		f.write(z)
-		f.write(")\n")
+	for i in fanins:
+		if types[i] != 'INPUT' and types[i] != 'NOT':
+			f.write("%s = %s("%(i,types[i]))
+			z = ''
+			for i in fanins[i]:
+				z = z + str(i) + ', '
+			z = z[:-2]
+			f.write(z)
+			f.write(")\n")
 
-	if types[i] == 'NOT':
-		f.write("%s = %s("%(i,types[i]))
-		f.write(str(fanins[i]))
-		f.write(")\n")
+		if types[i] == 'NOT':
+			f.write("%s = %s("%(i,types[i]))
+			f.write(str(fanins[i]))
+			f.write(")\n")
 
-f.write("\n")
+	f.write("\n")
+
+	m = m + 1
 
 
