ec6bf8ada0656544bd9b022936e34a0d57517085
Anirudh <anirudhlanka@Anirudh.local>
2018-07-07 17:06:37 +0530

    Added files

--- benchmarks/sarlock/in_upper/small/opt.sh
+++ benchmarks/sarlock/in_upper/small/opt.sh
@@ -2,13 +2,13 @@
 for i in *.bench
 do
 	exec 3< cmd.txt
-	a=$(echo "~/abc/abc -c "read_bench $i"")
+	a=$(echo '~/Documents/abc/abc -c' '"read_bench '$i'"')
 	while read 0<&3 line;
 	do
 		b=$(echo ${line})
 	done
 	exec 3<&-
 	c=$(echo $i | head -n1 | cut -d "_" -f1 )
-	echo  $a $b '-c "write_bench -l' $c'_opt.bench"'
+	$a $b '-c "write_bench -l' $c'_opt.bench"'
 done
 
