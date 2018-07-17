ec131754a4c0bc31cd531862c8995a3c7a0ea3db
Pramod Subramanyan <pramod.subramanyan@gmail.com>
2018-07-17 18:08:42 +0530

    Minor edits.

--- benchmarks/sarlock/optimised/standard/data.py
+++ benchmarks/sarlock/optimised/standard/data.py
@@ -9,34 +9,31 @@ import random
 import sys
 import os.path
 
-features1 = []
-features2 = []
-test_ind = []
 i = 0
-o = 1
-visited = set()
-types = {}
+ntypes  = {}
 ntypes1 = {}
 ntypes2 = {}
 nfanins = {}
-noutputs = set()
+tag = {}
 zgats = set()
 
 def dfs(n, depth, visited):
-    if depth == 0 or n in visited :
+    if n in visited :
         return
     visited.add(n)
+    if depth == 0:
+        return
     for f in nfanins[n]:
         dfs(f,depth-1,visited)
 
-for z in glob.glob(os.path.join(sys.argv[1], "*.bench")):
-    print(o," : ",z)
-    te = z[-10:]
-    file = open(z, "r")
+files = glob.glob(os.path.join(sys.argv[1], "*.bench"))
+for cnt, filename in enumerate(files):
+    print(cnt+1," : ",filename)
+    is_test = filename.endswith('test.bench')
+    file = open(filename, "r")
 
     fanins = {}
     maps = {}
-    outputs = set()
 
     l = file.readlines()
     for x in l:
@@ -44,31 +41,21 @@ for z in glob.glob(os.path.join(sys.argv[1], "*.bench")):
         if 'INPUT' in x:
             y = x.replace("INPUT(", "").replace(")", "").replace("\n", "")
             if 'keyinput' in y:
-                types[y] = 'keyinput'
-                if te == "test.bench":
-                    ntypes1[i] = 'keyinput'
-                else:
-                    ntypes2[i] = 'keyinput'
+                ntypes[i] = 'keyinput'
             else:
-                types[y] = 'input'
-                if te == "test.bench":
-                    ntypes1[i] = 'input'
-                else:
-                    ntypes2[i] = 'input'
+                ntypes[i] = 'input'
             fanins[y] = []
             nfanins[i] = []
             maps[y] = i
+            tag[i] = 'test' if is_test else 'train'
             i = i + 1
-
         elif 'OUTPUT' in x:
             y = x.replace("OUTPUT(", "").replace(")", "").replace("\n", "")
-            outputs.add(y)
 
         elif '=' in x:
             parts = [p.strip() for p in x.split('=')]
             if parts[1] != 'vdd':
                 gate_name = parts[1][:parts[1].find('(')]
-                types[parts[0]] = gate_name
                 within_br = parts[1][parts[1].find('(')+1:parts[1].find(')')]
                 f = [p.strip() for p in within_br.split(',')]
                 fanins[parts[0]] = f
@@ -76,85 +63,74 @@ for z in glob.glob(os.path.join(sys.argv[1], "*.bench")):
                 nf = []
                 for x in f:
                     nf = nf + [maps[x]]
-                if te == "test.bench":
-                    ntypes1[i] = gate_name
-                else:
-                    ntypes2[i] = gate_name
+                ntypes[i] = gate_name
                 nfanins[i] = nf
             else:
-                types[parts[0]] = 'vdd'
                 fanins[parts[0]] = []
                 maps[parts[0]] = i
-                if te == "test.bench":
-                    ntypes1[i] = 'vdd'
-                else:
-                    ntypes2[i] = 'vdd'
+                ntypes[i] = 'vdd'
                 nfanins[i] = []
+            tag[i] = 'test' if is_test else 'train'
             i = i + 1
 
     zgats.add(maps['ZGAT'])
-    for x in outputs:
-        noutputs.add(maps[x])
-    o += 1
 
-
-for j in sorted(ntypes1.keys()):
-    if ntypes1[j] == 'AND' or ntypes1[j] == 'and':
-        features1.append([0,2,0,0,0,1])
-    elif ntypes1[j] == 'NOT' or ntypes1[j] == 'not':
-        features1.append([1,1,1,1,0,0])
-    elif ntypes1[j] == 'INPUT' or ntypes1[j] == 'input':
-        features1.append([2,0,0,0,1,1])
-    elif ntypes1[j] == 'KEYINPUT' or ntypes1[j] == 'keyinput':
-        features1.append([3,0,0,0,1,1])
-    else:
-        features1.append([4,0,0,0,0,0])
-
-for j in sorted(ntypes2.keys()):
-    if ntypes2[j] == 'AND' or ntypes2[j] == 'and':
-        features2.append([0,2,0,0,0,1])
-    elif ntypes2[j] == 'NOT' or ntypes2[j] == 'not':
-        features2.append([1,1,1,1,0,0])
-    elif ntypes2[j] == 'INPUT' or ntypes2[j] == 'input':
-        features2.append([2,0,0,0,1,1])
-    elif ntypes2[j] == 'KEYINPUT' or ntypes2[j] == 'keyinput':
-        features2.append([3,0,0,0,1,1])
-    else:
-        features2.append([4,0,0,0,0,0])
-
-labels1 = np.ndarray(shape=([len(ntypes1),3]),dtype=int)
-labels2 = np.ndarray(shape=([len(ntypes2),3]),dtype=int)
-
+# Mark ZGAT and related nodes.
+visited = set()
 for z in zgats:
     dfs(z,3,visited)
     visited.remove(z)
 
-c1 = 0
-c2 = 0
-c3 = 0
+features1, features2 = [], []
+labels1, labels2 = [], []
+c1, c2, c3 = 0, 0, 0
 
-for i, j in enumerate(sorted(ntypes1.keys())):
-    if j in zgats: 
-        labels1[i] = [1,0,0]
+total_positive = float(len(visited) + len(zgats))
+undersampling_ratio = total_positive / (float(len(ntypes)) - total_positive)
+
+for i in ntypes.keys():
+    if tag[i] == 'test':
+        features, labels = features1, labels1
+    else:
+    #elif i in zgats or i in visited or random.random() < undersampling_ratio:
+        features, labels = features2, labels2
+    #else:
+    #    continue
+    
+    if ntypes[i].upper() == 'AND':
+        features.append([0,2,0,0,0,1])
+    elif ntypes[i].upper() == 'NOT':
+        features.append([1,1,1,1,0,0])
+    elif ntypes[i].upper() == 'INPUT':
+        features.append([2,0,0,0,1,1])
+    elif ntypes[i].upper() == 'KEYINPUT':
+        features.append([3,0,0,0,1,1])
+    else:
+        features.append([4,0,0,0,0,0])
+
+    if i in zgats: 
+        labels.append([1,0,0])
         c1 += 1
-    elif j in visited: 
-        labels1[i] = [0,1,0]
+    elif i in visited: 
+        labels.append([0,1,0])
         c2 += 1
     else: 
-        labels1[i] = [0,0,1]
+        labels.append([0,0,1])
         c3 += 1
 
-for i, j in enumerate(sorted(ntypes2.keys())):
-    if j in zgats: labels2[i] = [1,0,0]
-    elif j in visited: labels2[i] = [0,1,0]
-    else: labels2[i] = [0,0,1]
+def to_ndarray(l,dtype=int):
+    assert len(l) > 0 and len(l[0]) > 0
+    rows = len(l)
+    cols = len(l[0])
+    arr = np.ndarray(shape=(rows, cols), dtype=int)
+    for i, row in enumerate(l):
+        arr[i] = row
+    return arr
 
-
-feat1 = csr_matrix(features1)
-feat2 = csr_matrix(features2)
-
-feat_csr1 = csr_matrix.astype(feat1,np.float32)
-feat_csr2 = csr_matrix.astype(feat2,np.float32)
+labels1 = to_ndarray(labels1)
+labels2 = to_ndarray(labels2)
+feat_csr1 = csr_matrix.astype(csr_matrix(features1),np.float32)
+feat_csr2 = csr_matrix.astype(csr_matrix(features2),np.float32)
 
 x = open("ind.logdec.x","wt")
 tx = open("ind.logdec.tx","wt")
@@ -183,6 +159,7 @@ ty.close()
 graf.close()
 
 with open('ind.logdec.test.index', 'wt') as f:
-    for i in ntypes1:
-        print ('%d' % i, file=f)
+    for i in tag:
+        if tag[i] == 'test':
+            print ('%d' % i, file=f)
 
