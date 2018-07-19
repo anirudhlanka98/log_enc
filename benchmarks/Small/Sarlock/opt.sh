ef784dd753df9e1616c8e4fa53017d72225ba445
Anirudh <anirudhlanka@Anirudh.local>
2018-07-19 14:51:24 +0530

    Bug fixes

--- benchmarks/Small/Sarlock/opt.sh
+++ benchmarks/Small/Sarlock/opt.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
-x=1
-while [ $x -le 40 ]
+for z in *
 do
-	z="small-"$x
-	cd $z
-	for i in *.bench
-	do
-		exec 3< ../cmd.txt
-		a=$(echo '~/Documents/abc/abc -c' '"read_bench '$i'"')
-		while read 0<&3 line;
+	if [ -d $z ];then
+		cd $z
+		for i in *.bench
 		do
-			b=$(echo ${line})
+			exec 3< ../cmd.txt
+			a=$(echo '~/Documents/abc/abc -c' '"read_bench '$i'"')
+			while read 0<&3 line;
+			do
+				b=$(echo ${line})
+			done
+			exec 3<&-
+			c=$(echo $i | head -n1 | cut -d "_" -f1 )
+			d=$(echo $i | head -n1 | cut -d "-" -f2 )
+			g=$(echo $a $b '-c "write_bench -l' $c'_opt-'$d'"')
+			eval $g
 		done
-		exec 3<&-
-		c=$(echo $i | head -n1 | cut -d "_" -f1 )
-		d=$(echo $i | head -n1 | cut -d "-" -f3 )
-		g=$(echo $a $b '-c "write_bench -l' $c'_opt-'$d'"')
-		eval $g
-	done
-	mkdir $z"_opt"
-	for m in *_opt-*.bench
-	do
-		mv $m $z"_opt"
-	done
-	cd ../
-	x=$(( $x + 1 ))
+		mkdir $z"_opt"
+		for m in *_opt-*.bench
+		do
+			mv $m $z"_opt"
+		done
+		mv $z"_opt" ../
+		cd ../
+	fi
 done
 	
 
